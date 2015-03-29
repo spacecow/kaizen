3a5c03200fa5b703b9f7cd1b7dd3679724586a22
Johan Sveholm <jsveholm@gmail.com>
2015-03-29 13:44:02 +0900

    vagrant

---
+++ rbenv.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+sudo apt-get install -y libssl-dev libffi-dev
+
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+rbenv_path='export PATH="$HOME/.rbenv/bin:$PATH"'
+eval $rbenv_path
+echo $rbenv_path >> ~/.bashrc
+rbenv_init='eval "$(rbenv init -)"'
+eval $rbenv_init
+echo $rbenv_init >> ~/.bashrc
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+cd /vagrant
+ruby_version=`cat .ruby-version`
+rbenv install $ruby_version 
+rbenv rehash
+rbenv global $ruby_version 
